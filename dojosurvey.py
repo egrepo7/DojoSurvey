d4b7f1fb5b5213fab42567eac91e742025572af3
Edgar Grepo <egrepo7@gmail.com>
2016-05-04 15:58:26 -0700

    first commit

--- dojosurvey.py
+++ dojosurvey.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 app = Flask (__name__)
+app.secret_key = 'SHHHHH'
 
 @app.route('/')
 def index():
@@ -7,10 +8,22 @@ def index():
 
 @app.route('/survey' , methods = ['POST'])
 def create_user():
+
+	if len(request.form['name']) < 1:
+		flash('Name cannot be empty!')
+		return redirect('/')
+	elif len(request.form['comments']) > 120:
+		flash('Comments must be less than 120 characters')
+		return redirect('/')
+	else:
+		flash('')
+		return render_template('result.html' , name = request.form['name'], dojo = request.form['dojo'], language = request.form['language'], comments = request.form['comments'])
+
+
 		# name = request.form['name']
 		# dojo = request.form['dojo']
 		# language = request.form['language']
 		# comments = request.form['comments']
-		return render_template('result.html' , name = request.form['name'], dojo = request.form['dojo'], language = request.form['language'], comments = request.form['comments'])
+	return render_template('result.html' , name = request.form['name'], dojo = request.form['dojo'], language = request.form['language'], comments = request.form['comments'])
 
 app.run(debug=True)
